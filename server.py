26d604ea38adea787e9758ab41576b3f6f6003ba
mecaneer23 <mecaneer23@gmail.com>
2023-12-17 15:09:02 -0600

    feature: render qrcode

--- server.py
+++ server.py
@@ -4,17 +4,30 @@ Main server file. Runs a flask server which can be used to upload and download f
 """
 # pylint: disable=missing-function-docstring
 
-from flask import Flask, render_template, redirect
+from socket import AF_INET
+
+from flask import Flask, redirect, render_template
+from werkzeug.serving import get_interface_ip
+from flask_qrcode import QRcode
+
+
+HOSTNAME = "0.0.0.0"
+PORT = 8000
+IP = f"http://{get_interface_ip(AF_INET)}:{PORT}"
 
 app = Flask(__name__)
+QRcode(app)
+
 
 @app.route("/")
 def root():
-    return render_template("index.html")
+    return render_template("index.html", ip=IP)
+
 
 @app.errorhandler(404)
 def page_not_found(error):
     _ = error
     return redirect("/")
 
-app.run(host="0.0.0.0", port=8000, debug=True)
+
+app.run(host=HOSTNAME, port=PORT, debug=True)

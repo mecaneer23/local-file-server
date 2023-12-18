ab1ea44bf431869f3dcf79e2cab77cc9c7a8bff4
mecaneer23 <mecaneer23@gmail.com>
2023-12-18 16:58:21 -0600

    hey it works!

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ Main server file. Runs a flask server which can be used to upload and download f
 
 from socket import AF_INET
 
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, jsonify
 from flask_qrcode import QRcode
 from werkzeug.serving import get_interface_ip
 from werkzeug.utils import secure_filename
@@ -34,9 +34,9 @@ def root():
 @app.route("/upload", methods=["POST"])
 def upload():
     file = request.files["file"]
-    print(file)
-    file.save(f"files/{secure_filename(file.filename)}")
-    # return redirect("/")
+    if file:
+        file.save(f"files/{secure_filename(file.filename)}")
+    return redirect("/")
 
 
 if __name__ == "__main__":

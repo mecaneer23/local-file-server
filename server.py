9362519f36e729f8aeb7ae30efe58108ac92fbff
mecaneer23 <mecaneer23@gmail.com>
2024-01-08 22:10:57 -0600

    feature: migrate to pathlib

--- server.py
+++ server.py
@@ -4,47 +4,64 @@ Main server file. Runs a flask server which can be used to upload and download f
 """
 # pylint: disable=missing-function-docstring
 
-import os
+from pathlib import Path
 from socket import AF_INET
 
-from flask import Flask, redirect, render_template, request, flash, send_from_directory
+from flask import (
+    Flask,
+    flash,
+    redirect,
+    render_template,
+    request,
+    send_from_directory,  # pyright: ignore
+)
 from flask_qrcode import QRcode
+from werkzeug.wrappers import Response
 from werkzeug.serving import get_interface_ip
 from werkzeug.utils import secure_filename
 
 HOSTNAME = "0.0.0.0"
 PORT = 8000
 IP = f"http://{get_interface_ip(AF_INET)}:{PORT}"
+FOLDER = "files"
 
 app = Flask(__name__)
 app.secret_key = "Some"
 QRcode(app)
+local_path = Path(__file__).parent
 
 
 @app.route("/")
 def root():
-    return render_template("index.html", ip=IP, files=os.listdir("files/"))
+    return render_template(
+        "index.html",
+        ip=IP,
+        files=map(lambda path: path.name, local_path.joinpath(FOLDER).iterdir()),
+    )
 
 
-@app.route('/files/<path:filename>', methods=['GET', 'POST'])
-def download(filename):
-    return send_from_directory("files", filename)
+@app.route(f"/{FOLDER}/<path:filename>", methods=["GET", "POST"])
+def download(filename: str) -> Response:
+    return send_from_directory(local_path.joinpath(FOLDER), filename)
 
 
 @app.route("/upload", methods=["POST"])
-def upload():
+def upload() -> Response:
     file = request.files["file"]
-    path = f"files/{secure_filename(file.filename)}"
+    if file.filename is None:
+        flash("No file provided")
+        return redirect("/")
+    path = local_path.joinpath(FOLDER, secure_filename(str(file.filename)))
     if not file:
         flash("No file provided")
         return redirect("/")
-    if os.path.exists(os.path.join(path)):
+    if path.exists():
         flash("Filename already exists on server")
         return redirect("/")
-    file.save(path)
+    file.save(path)  # pyright: ignore
     flash(f"{file.filename} was uploaded successfully")
     return redirect("/")
 
 
 if __name__ == "__main__":
-    app.run(host=HOSTNAME, port=PORT, debug=False)
+    app.run(host=HOSTNAME, port=PORT, debug=True)
---
+++ typings/flask_qrcode.pyi
@@ -0,0 +1,8 @@
+"""Stubs for flask qrcode"""
+# pylint: disable=missing-class-docstring
+
+from flask import Flask
+
+class QRcode:
+    def __init__(self, app: Flask) -> None:
+        ...
